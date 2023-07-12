aabe8173ea60413513442d80bbc9dde7630031bb
carmenb2020 <carmenborjap@hotmail.com>
2023-07-12 19:10:52 +0200

    finished test class Soldier

--- src/main/java/org/example/soldier/Soldier.java
+++ src/main/java/org/example/soldier/Soldier.java
@@ -1,4 +1,22 @@
 package org.example.soldier;
 
 public class Soldier {
+    int health;
+    int strength;
+
+    public Soldier(int health, int strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+
+    public int attack() {
+        int strength1 = this.strength;
+        return (strength1);
+    }
+
+    public int receiveDamage(int damage) {
+        this.health = this.health - damage;
+        int health1 = this.health;
+        return(health1);
+    }
 }
